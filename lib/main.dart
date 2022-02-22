40844eed9d510824fbd891332b616b9ab6e3755a
rooky0509 <rooky0509@gmail.com>
2022-02-22 09:34:29 +0900

    모두리셋 / Provider사용

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:ontime/home.dart';
+import 'package:provider/provider.dart';
+import 'package:ontime/providers/time.dart';
+import 'package:ontime/widgets/time.dart';
 
 void main() {
-  runApp(MyApp());
-
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => Providerr()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
+        primarySwatch: Colors.blue,
       ),
       home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Provider'),
+      ),
+      body: Column(
+        children: <Widget>[
+          ChangeNotifierProvider(
+        create: (BuildContext context) => Providerr(),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Counter(),
+              Counter(),
+              Buttons(),
+            ],
+          ),
+        ),
+      ),
+      ChangeNotifierProvider(
+        create: (BuildContext context) => Providerr(),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Counter(),
+              Buttons(),
+            ],
+          ),
+        ),
+      ),
+        ],
+      )
     );
   }
 }
---
+++ lib/widgets/time.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:ontime/providers/time.dart';
+
+class Counter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print('Counter');
+
+    return Text(
+      context.watch<Providerr>().count.toString(),
+      style: TextStyle(
+        fontSize: 20,
+      ),
+    );
+  }
+}
+
+class Buttons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+            onPressed: () {
+              context.read<Providerr>().add();
+            },
+            child: Icon(Icons.add)),
+        SizedBox(
+          width: 40,
+        ),
+        ElevatedButton(
+            onPressed: () {
+              context.read<Providerr>().remove();
+            },
+            child: Icon(Icons.remove))
+      ],
+    );
+  }
+}
