52c81bedc355a43adcdbdbee8d2a8f28f33f0783
rooky0509 <rooky0509@gmail.com>
2022-03-05 08:55:45 +0900

    조금 되돌림, disposse적용 필요

--- lib/pages/schedule.dart
+++ lib/pages/schedule.dart
@@ -7,6 +7,10 @@ import 'package:ontime/data/saveData.dart';
 import 'package:provider/provider.dart';
 
 
+  /*
+    값 읽기 : context.select((ScheduleProvider value) => value.count)
+    값 변경 : context.read<ScheduleProvider>().add();
+  */
 class Schedule {
   ScheduleProvider provider = ScheduleProvider();
   String title = "Schedule"; //제목
@@ -14,25 +18,23 @@ class Schedule {
   Widget page = SchedulePage(); //페이지 위젯
 }
 
-/* class SchedulePage extends StatelessWidget {
-  /*
-    값 읽기 : context.select((ScheduleProvider value) => value.count)
-    값 변경 : context.read<ScheduleProvider>().add();
-  */
+class SchedulePage extends StatefulWidget {
+  @override
+  _SchedulePageState createState() => _SchedulePageState();
+}
+class _SchedulePageState extends State<SchedulePage> {
+  @override
+  void dispose(){
+    print("Page Dispose");
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
+    print("SchedulePage build");
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            "SchedulePage!\n${context.select((ScheduleProvider value) => value.str).toString()}", // count를 화면에 출력
-            style: TextStyle(
-              fontSize: 40.0,
-              backgroundColor: Colors.amber,
-              color: Colors.blue,
-            ),
-          ),
           ElevatedButton(
             onPressed: () {
               context.read<ScheduleProvider>().add();
@@ -79,184 +81,71 @@ class Schedule {
       ),
     );
   }
-} */
-class SchedulePage extends StatefulWidget {
-  @override
-  _SchedulePageState createState() => _SchedulePageState();
-}
-class _SchedulePageState extends State<SchedulePage> {
-  bool load = false;
-  Widget build(BuildContext context) {
-    if(!(load)) context.read<ScheduleProvider>().timerOn(build:true,text: "빌드(_SchedulePageState):위젯생성 => 값을 넣음 => 타이머 최초실행").then((value)=>setState(()=>load = true));
-    return Scaffold(
-      body: Container(
-        padding: EdgeInsets.only(top: 100),
-        child: load?Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              flex: 1,
-              child: Container(
-                alignment: Alignment.center, 
-                color: Colors.amber,
-                child: Text(
-                  context.select((ScheduleProvider value){ //"${(value.datas.isNotEmpty)&(value.datas.length>value.cellIndex)&(value.cellIndex>=-1)?value.datas[(value.cellIndex==-1?value.datas.length:value.cellIndex)-1]["id"]:''}"
-                    if(value.datas.isEmpty) return "x";
-                    if(value.cellIndex==-1) return value.datas.last.toString();
-                    if(value.cellIndex==0) return "min";
-                    return value.datas[value.cellIndex-1].toString();
-                  })
-                )
-              )
-            ),
-            Expanded(
-              flex: 4,
-              child: Container(
-                alignment: Alignment.center,
-                color: Colors.red,
-                child: context.select((ScheduleProvider value){
-                  int index = value.cellIndex;
-                  if(value.datas.isEmpty){ //등록된 데이터가 없음
-                    return Text("등록된 수업 없음 $index");
-                  }
-                  if(index == -1){ //뒤에있는 end가 없음
-                    return Text("수업 끝남 $index");
-                  }
-                  Map data = value.datas[index];
-                  if(!value.onGoing){ //뒤에있는 end와 앞에있는 start가 같지 않음
-                    return Text("다음수업 : $index\n::${DateTime.now().setTime(second: data['id']%86400).format()} ~ ${DateTime.now().setTime(second: data['id']%86400+data['len']).format()}");
-                  }
-                  return Column(
-                    children: <Widget>[
-                      Text(
-                        data["label"]
-                      ),
-                      Text(
-                        data["title"]
-                      ),
-                      Text(
-                        data["detail"]
-                      ),
-                      Text(
-                        data["tag"]
-                      ),
-                      Text(
-                        "${DateTime.now().setTime(second: data['id']%86400).format()} ~ ${DateTime.now().setTime(second: data['id']%86400+data['len']).format()}"
-                      ),
-                    ],
-                  );
-                }),
-                /*  */
-              )
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(alignment: Alignment.center, color: Colors.amber,child: Text(
-                context.select((ScheduleProvider value){ //"${(value.datas.isNotEmpty)&(value.datas.length>value.cellIndex)&(value.cellIndex>=-1)?value.datas[(value.cellIndex==-1?value.datas.length:value.cellIndex)-1]["id"]:''}"
-                  if(value.datas.isEmpty) return "x";
-                  if(value.cellIndex==-1) return "-";
-                  if(value.cellIndex+1==value.datas.length) return "MAX";
-                  return value.datas[value.cellIndex+1].toString();
-                }).toString()
-              ),)
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(alignment: Alignment.center, color: Colors.blue,child: Text(
-                context.select((ScheduleProvider value) => (value.now as DateTime).format(newPattern: "HH:mm:ss.mmm"))
-              ),)
-            ),
-              ElevatedButton(
-                onPressed: () {
-                  context.read<ScheduleProvider>().add();
-                },
-                child: Icon(Icons.add,)
-              ),
-              SizedBox(
-                width: 40
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  context.read<ScheduleProvider>().remove();
-                },
-                child: Icon(Icons.remove)
-              ),
-              SizedBox(
-                width: 40,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  context.read<ScheduleProvider>().save();
-                },
-                child: Icon(Icons.save)
-              ),
-              SizedBox(
-                width: 40,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  context.read<ScheduleProvider>().get();
-                },
-                child: Icon(Icons.get_app)
-              ),
-              SizedBox(
-                width: 40,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  context.read<ScheduleProvider>().clear();
-                },
-                child: Icon(Icons.delete)
-              ),
-          ],
-        ):CircularProgressIndicator(),
-      
-      )
-    );
-  }
 }
 class ScheduleWidget extends StatefulWidget {
   @override
   _ScheduleWidgetState createState() => _ScheduleWidgetState();
 }
 class _ScheduleWidgetState extends State<ScheduleWidget> {
-
-  bool load = false;
+  @override
+  void dispose(){
+    print("Widget Dispose");
+    super.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
-    if(!(load)) context.read<ScheduleProvider>().timerOn(build:true,text: "빌드(_ScheduleWidgetState):위젯생성 => 값을 넣음 => 타이머 최초실행").then((value)=>setState(()=>load = true));
-    return load?Column(
+    print("ScheduleWidget build");
+    return FutureBuilder(
+      future: context.read<ScheduleProvider>().load(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData == false) {
+          return CircularProgressIndicator();
+        }
+        else if (snapshot.hasError) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Error: ${snapshot.error}',
+              style: TextStyle(fontSize: 15),
+            ),
+          );
+        }
+        else {
+          return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Expanded(
+/*P*/   Expanded(
           flex: 1,
           child: Container(
             alignment: Alignment.center, 
             color: Colors.amber,
             child: Text(
-              context.select((ScheduleProvider value){ //"${(value.datas.isNotEmpty)&(value.datas.length>value.cellIndex)&(value.cellIndex>=-1)?value.datas[(value.cellIndex==-1?value.datas.length:value.cellIndex)-1]["id"]:''}"
-                if(value.datas.isEmpty) return "x / 리스트가 비어있음";
-                if(value.cellIndex==-1) return value.datas.last.toString();
+              context.select((ScheduleProvider value){
+                List datas = value.weekData;
+                if(datas.isEmpty) return "x / 리스트가 비어있음";
+                if(value.cellIndex==-1) return datas.last.toString();
                 if(value.cellIndex==0) return "min / 이전 수업이 없음";
-                return value.datas[value.cellIndex-1].toString();
+                return datas[value.cellIndex-1].toString();
               })
             )
           )
         ),
-        Expanded(
+/*C*/   Expanded(
           flex: 4,
           child: Container(
             alignment: Alignment.center,
             color: Colors.red,
             child: context.select((ScheduleProvider value){
               int index = value.cellIndex;
-              if(value.datas.isEmpty){ //등록된 데이터가 없음
+              List datas = value.weekData;
+              if(datas.isEmpty){ //등록된 데이터가 없음
                 return Text("등록된 수업 없음 $index");
               }
               if(index == -1){ //뒤에있는 end가 없음
                 return Text("수업 끝남 $index");
               }
-              Map data = value.datas[index];
-              DateTime now = DateTime.now();
+              Map data = datas[index];
+              DateTime now = value.now!;
               DateTime start = now.setTime(second: data['id']%86400);
               DateTime end = start.setTime(second: data['id']%86400 + data['len']);
               if(!value.onGoing){ //뒤에있는 end와 앞에있는 start가 같지 않음
@@ -264,11 +153,14 @@ class _ScheduleWidgetState extends State<ScheduleWidget> {
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
                     Text(
-                      "다음수업은 "+data["title"]+"입니다",
+                      "다음수업은 ${data["title"]}입니다",
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.bold
                       ),
+                    ),
+                    Text(
+                      "${data["id"]%86400} / ${data["id"]~/86400}"
                     ),
                     Text(
                       "${start.format()} ~ ${end.format()}"
@@ -311,7 +203,7 @@ class _ScheduleWidgetState extends State<ScheduleWidget> {
                     "${start.format()} ~ ${end.format()}"
                   ),
                   Text(
-                    "${now.setTime(second: end.difference(now).inSeconds).format(newPattern:"HH:mm:ss")}남음"
+                    "${now.setTime(second: end.difference(now).inSeconds).format(newPattern:"HH:mm:ss")}남음" + now.format(newPattern: "HH:mm:ss.mmm")
                   ),
                 ],
               );
@@ -319,30 +211,29 @@ class _ScheduleWidgetState extends State<ScheduleWidget> {
             /*  */
           )
         ),
-        Expanded(
+/*N*/   Expanded(
           flex: 1,
           child: Container(alignment: Alignment.center, color: Colors.amber,child: Text(
             context.select((ScheduleProvider value){ //"${(value.datas.isNotEmpty)&(value.datas.length>value.cellIndex)&(value.cellIndex>=-1)?value.datas[(value.cellIndex==-1?value.datas.length:value.cellIndex)-1]["id"]:''}"
-              if(value.datas.isEmpty) return "x / 리스트가 비어있음";
+              List datas = value.weekData;
+              if(datas.isEmpty) return "x / 리스트가 비어있음";
               if(value.cellIndex==-1) return "- / 모든 수업이 끝남";
-              if(!value.onGoing) return value.datas[value.cellIndex].toString();
-              if(value.cellIndex+1==value.datas.length) return "MAX / 다음 수업이 없음";
-              return value.datas[value.cellIndex+1].toString();
+              if(!value.onGoing) return datas[value.cellIndex].toString();
+              if(value.cellIndex+1==datas.length) return "MAX / 다음 수업이 없음";
+              return datas[value.cellIndex+1].toString();
             }).toString()
           ),)
-        ),/* 
-        Expanded(
-          flex: 1,
-          child: Container(alignment: Alignment.center, color: Colors.blue,child: Text(
-            context.select((ScheduleProvider value) => (value.now as DateTime).format(newPattern: "HH:mm:ss.mmm"))
-          ),)
-        ), */
+        ),
       ],
-    ):CircularProgressIndicator();
+    );
+        }
+      }
+    ); 
   }
 }
 
 class ScheduleProvider with ChangeNotifier {
+  //#region SQLite
   final SaveData _saveData = SaveData(tableName: "schedule", tableAttributede: {
     "id" : "INTEGER PRIMARY KEY",
     "len" : "INTEGER",
@@ -357,104 +248,65 @@ class ScheduleProvider with ChangeNotifier {
   String _title = "";
   String _detail = "";
   String _tag = "";
-
+  //#endregion
+  
   int _start = 0;//, week = 0;
-  List datas = [];
+  List datas = [], weekData = [];
   int cellIndex = -1, _weekIndex = 0, _selectWeekIndex = 0;
   bool onGoing = false;
   Timer? _timer;
   DateTime? now;
 
-  String _str = ""; //테슽트용 텍스트
-  String get str => _str;
+  Future<bool> load() async{
+    datas = await get();
+    timerOn();
+    print(datas);
+    return datas.isNotEmpty;
+  }
 
-  Future<void> timerOn({String text = "정보없음", bool? build}) async{
-    print("[ScheduleProvider] [[$text]] => 타이머 실행");
-    if(build??false) {
-      await get();
-    }
-    _timer = Timer.periodic(Duration(milliseconds: 10), (timer) { 
-      print("[ScheduleProvider] 타이머 실행중");
+  void timerOn() {
+    _timer = Timer.periodic(Duration(milliseconds: 1000), (timer) { 
       now = DateTime.now();
       _weekIndex = now!.weekday-1;
-      cellIndex = datas.indexWhere((element)=>(element["id"]%86400+element["len"] > now!.toSec())); // next end
-      onGoing = cellIndex==-1?false:(datas[cellIndex]["id"]%86400 < now!.toSec())&(datas[cellIndex]["id"]%86400+datas[cellIndex]["len"] > now!.toSec());
-      if(datas.isEmpty) {
-        timerOff(text : "데이터가 비어있음 => 타이머종료");
-      }else if(cellIndex == -1) {
-        timerOff(text : "인덱스가 -1임 == 모든수업끝시간보다 늦음 => 타이머종료");
-      }
 
-
-      _start = now!.toSec(); //test
-
-
+      weekData = datas.where((element) => ((86400*_weekIndex)<=element["id"]) & (element["id"]<(86400 * (_weekIndex+1))) ).toList();
+      cellIndex = weekData.indexWhere((element)=>(now!.toSec() < element["id"]%86400+element["len"])); // next end
+      onGoing = cellIndex==-1?false:(weekData[cellIndex]["id"]%86400 <= now!.toSec())&(now!.toSec() < weekData[cellIndex]["id"]%86400+weekData[cellIndex]["len"]);
+      
       notifyListeners();
     });
   }
 
-  void timerOff({String text = "정보없음"}) {
-    print("[ScheduleProvider] [[$text]] => 타이머 종료");
+  void timerOff() {
     _timer!.cancel();
     notifyListeners();
   }
   
   void add() {
     print("[ScheduleProvider] add");
-    now = DateTime.now(); //now.hour*3600 + now.minute*60 + now.second
-    _start = now!.toSec();
-    _weekIndex = now!.weekday-1;
-
-    _id = _start + (86400 * _weekIndex);// [0~86399] + [0, 86400, 86400*2, ...] = [0~86399,86400~172,799]
-    _label = "1교시";
-    _title = "Title";
-    _detail = "Detail";
-    _tag = "학교";
-
-    print("[ScheduleProvider] ${now!.weekday}/$_start = ${now!.setTime(second: _start)}");
     notifyListeners();
   }
 
   void remove() {
     print("[ScheduleProvider] remove");
-    _len += 10;
-    print("[ScheduleProvider] $_len");
     notifyListeners();
   }
 
   Future<void> save() async{ // →DB( class => Map => save )
-
-    now = DateTime.now(); //now.hour*3600 + now.minute*60 + now.second
-    _start = now!.toSec();
-    _weekIndex = now!.weekday-1;
-
-    _id = _start + (86400 * _weekIndex) + _len;// [0~86399] + [0, 86400, 86400*2, ...] = [0~86399,86400~172,799]
-    _label = "1교시";
-    _title = "T [$_id]";
-    _detail = "Detail";
-    _tag = "학교";
-
     print("[ScheduleProvider] ${now!.weekday}/$_start = ${now!.setTime(second: _start)}");
-
-
     print("[ScheduleProvider] save");
     _saveData.INSERT(data: {
-      "id" : _id,
-      "len" : _len,
-      "label" : "${_len/10}교시",
-      "title" : _title,
-      "detail" : "$_detail : $_weekIndex",
-      "tag" : _tag,
+      "id" : now!.setTime(second: _start).toSec(),//_id,
+      "len" : 20,//_len,
+      "label" : "label${now!.format()}",//_label,
+      "title" : "label${now!.format()}",//_title,
+      "detail" : "label${now!.format()}",//_detail,
+      "tag" : "학교",//_tag,
     }).then((value) => print("[ScheduleProvider] ------save : FINISH"));
   }
 
-  Future<void> get() async{ // ←DB( get => Map => calss )
-    _saveData.SELECT(orderKey: "id").then((value){
-      datas = value;
-      timerOn(text:"데이터를 받음 => 타이머실행");
-      notifyListeners(); 
-      print("[ScheduleProvider] getAll[${value.length}]"); //test
-    });
+  Future<List<Map<String, dynamic>>> get() async{ // ←DB( get => Map => calss )
+    return await _saveData.SELECT(orderKey: "id");
   }
 
   Future<void> clear()async=>_saveData.DELETE();
